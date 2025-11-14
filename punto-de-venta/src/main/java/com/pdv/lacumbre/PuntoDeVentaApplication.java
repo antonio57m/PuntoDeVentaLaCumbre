62494d593a77917f334a1866182c25dd4d48f4a8
antonio57m <antonioguerrerom57@gmail.com>
2025-11-14 00:26:46 -0600

    Inicializacion del proyecto

---
+++ punto-de-venta/src/main/java/com/pdv/lacumbre/PuntoDeVentaApplication.java
@@ -0,0 +1,13 @@
+package com.pdv.lacumbre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuntoDeVentaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuntoDeVentaApplication.class, args);
+	}
+
+}
---
+++ punto-de-venta/src/test/java/com/pdv/lacumbre/PuntoDeVentaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdv.lacumbre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuntoDeVentaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
